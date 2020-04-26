ef1514aee8cd9667fd3fa1f25033bc68c0d1bb1d
Cristiano Oliveira <cristiano.oliveira@inloco.com.br>
2020-04-26 18:30:00 -0300

    Tree

--- project/src/algorithms/datastructures/Tree.kt
+++ project/src/algorithms/datastructures/Tree.kt
@@ -1,5 +1,17 @@
 package algorithms.datastructures
 
-class Tree <T> (var value: T?) {
+class Tree<T>(var value: T?) {
+    val children: MutableList<Tree<T>> = mutableListOf()
 
+    fun size(): Int {
+        return children.fold(1, { size, child -> size + child.size() })
+    }
+    
+    fun height(): Int {
+        return 1 + (children.map { it.size() }.max() ?: 0)
+    }
+
+    fun add(value: T) {
+        children.add(Tree(value))
+    }
 }
